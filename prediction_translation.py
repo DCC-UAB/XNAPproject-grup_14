998cbb4b84128fc62cd7f78d0ea39795de7ce12c
llucvicented <130971223+llucvicented@users.noreply.github.com>
2024-05-16 21:25:18 +0200

    Update prediction_translation.py

--- prediction_translation.py
+++ prediction_translation.py
@@ -3,7 +3,7 @@ from util import *
 filename="char2encoding.pkl"
 sentence="We are doing language translation"
 input_token_index,max_encoder_seq_length,num_encoder_tokens,reverse_target_char_index,num_decoder_tokens,target_token_index= getChar2encoding(filename)
-encoder_input_data=encodingSentenceToPredict(sentence,input_token_index,16,71)
+encoder_input_data=encodingSentenceToPredict(sentence,input_token_index,max_encoder_seq_length,num_encoder_tokens)
 encoder_model= load_model('encoder_modelPredTranslation.h5')
 decoder_model= load_model('decoder_modelPredTranslation.h5')
 
